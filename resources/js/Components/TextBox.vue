9c15d5930d61277d6f44746946617cccad204fa1
harvicapino22 <harvicapino22@gmail.com>
2023-02-18 19:05:15 +0800

    added Form Components

---
+++ resources/js/Components/TextBox.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    modelValue: String,
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <input 
+        :value="modelValue"
+        @change="$emit('update:modelValue', $event.target.value)"
+        class="block w-full rounded-lg text-sm px-4 py-2 bg-slate-700 border border-gray-500
+                focus:ring-1 focus:outline-none focus:ring-blue-600 focus:border-blue-600"
+    />
+</template>
---
+++ resources/js/Layouts/Authentication.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <main class="flex flex-col justify-center items-center gap-8 py-14 min-h-screen">
+        <slot />
+    </main>
+</template>
