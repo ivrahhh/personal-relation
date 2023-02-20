bd9295269eb5f83ef2ae81562903e85ef1a52e2c
harvicapino22 <harvicapino22@gmail.com>
2023-02-20 20:32:09 +0800

    small fix in NavLink Component and Admin Layout

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -1,13 +1,23 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
-defineProps({
+const props = defineProps({
     label: String,
+    active: Boolean,
+})
+
+const activeStyle = computed(() => {
+    if(props.active) {
+        return 'bg-slate-900 text-white'
+    } else {
+        return 'hover:bg-gray-100'
+    }
 })
 </script>
 
 <template>
-    <Link class="flex items-center gap-1 text-sm px-4 py-3">
+    <Link class="flex items-center gap-1 text-sm px-4 py-2 rounded-lg" :class="activeStyle">
         <slot>{{ label }}</slot>
     </Link>
 </template>
--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -45,15 +45,15 @@ const defaultImage = computed(() => {
         </header>
         <div class="flex w-full grow">
             <aside class="shrink-0 bg-white shadow-md">
-                <nav class="flex flex-col py-4 sticky top-16 w-64">
-                    <NavLink :href="route('dashboard')">
+                <nav class="flex flex-col gap-2 p-4 sticky top-16 w-64">
+                    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
                         </svg>
                         Dashboard
                     </NavLink>
-                    <NavLink href="">
+                    <NavLink :href="route('contacts.index')" :active="route().current('contacts.*')">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                         </svg>
